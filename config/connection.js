1f0b2f0e97ca15babe983423f2e2fbab9687ca0b
Richard <rkj128@gmail.com>
2020-07-11 09:44:58 -0700

    Herkoku updates

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = myssql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+};
+
+
+
+
 
 connection.connect(function (err) {
     if (err) {
